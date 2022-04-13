7b91ab91364241c560eb8e2eba9a820e6cff0e18
Marius Geležinis <marius.gelezinis@devocean.lt>
2022-04-13 14:33:52 +0300

    Added modification client

---
+++ src/Http/ModificationClient.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DevOceanLT\Camunda\Http;
+
+use DevOceanLT\Camunda\Exceptions\CamundaException;
+
+class ModificationClient extends CamundaClient
+{
+    public static function execute(array $parameters): bool
+    {
+        $response = self::make()->post(
+            "modification/execute",
+            compact('parameters')
+        );
+
+        if ($response->status() === 204) {
+            return true;
+        }
+
+        throw new CamundaException($response->body(), $response->status());
+    }
+
+    public static function executeAsync(array $parameters): bool
+    {
+        $response = self::make()->post(
+            "modification/executeAsync",
+            compact('parameters')
+        );
+
+        if ($response->status() === 204) {
+            return true;
+        }
+
+        throw new CamundaException($response->body(), $response->status());
+    }
+}
